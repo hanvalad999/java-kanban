41f746dbbeacb5e9c80d929b3d6d7d6c164bcee3
Hanvalad <hanvaladbabaev@yandex.ru>
2025-07-23 11:43:47 +0300

    fix: добавлены отступы между методами для прохождения Checkstyle

--- src/manager/TaskManager.java
+++ src/manager/TaskManager.java
@@ -7,32 +7,44 @@ import model.Subtask;
 import java.util.List;
 
 public interface TaskManager {
+
     public Task createTask(Task task);
+
     public Epic createEpic(Epic epic);
+
     public Subtask createSubtask(Subtask subtask);
 
     public List<Task> getAllTasks();
+
     public List<Epic> getAllEpics();
+
     public List<Subtask> getAllSubtasks();
 
     public Task getTaskById(int id);
+
     public Epic getEpicById(int id);
+
     public Subtask getSubtaskById(int id);
 
     public void updateTask(Task updatedTask);
+
     public void updateEpic(Epic updatedEpic);
+
     public void updateSubtask(Subtask updatedSubtask);
 
     public void deleteTask(int id);
+
     public void deleteEpic(int id);
+
     public void deleteSubtask(int id);
 
     public void clearAllTasks();
+
     public void clearAllEpics();
+
     public void clearAllSubtasks();
 
     public List<Subtask> getSubtasksOfEpic(int epicId);
 
     public List<Task> getHistory();
-
 }
