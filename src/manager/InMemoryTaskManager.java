857cfadbc73b22e783c760c50f604d0bf07110f5
Hanvalad <hanvaladbabaev@yandex.ru>
2025-07-23 13:39:58 +0300

    Доработка метода deleteTask

--- src/manager/InMemoryTaskManager.java
+++ src/manager/InMemoryTaskManager.java
@@ -122,6 +122,7 @@ public class InMemoryTaskManager implements TaskManager {
     @Override
     public void deleteTask(int id) {
         tasks.remove(id);
+        historyManager.remove(id);
     }
 
     @Override
