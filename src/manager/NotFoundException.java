034d40502007b30837e71c95f86fa927c0e3152c
Hanvalad <hanvaladbabaev@yandex.ru>
2025-10-06 14:40:17 +0300

    Исправление стиля кода

--- src/manager/NotFoundException.java
+++ src/manager/NotFoundException.java
@@ -1,5 +1,8 @@
 package manager;
 
 public class NotFoundException extends RuntimeException {
-    public NotFoundException(String message) { super(message); }
+
+    public NotFoundException(String message) {
+        super(message);
+    }
 }
